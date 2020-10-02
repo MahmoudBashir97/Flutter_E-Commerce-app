9647277fb7f4d44cace8c94eb2d7d1dbe0686127
Mahmoud Bashir <anadeouf@yahoo.com>
2020-10-02 02:35:44 +0200

    Mahmoud commit

--- lib/home/components/body.dart
+++ lib/home/components/body.dart
@@ -1,15 +1,11 @@
-import 'package:e_commerce_app/constants.dart';
 import 'package:e_commerce_app/home/components/categories.dart';
 import 'package:e_commerce_app/home/components/discount_banner.dart';
 import 'package:e_commerce_app/home/components/home_header.dart';
 import 'package:e_commerce_app/home/components/popular_products.dart';
-import 'package:e_commerce_app/home/components/product_card.dart';
-import 'package:e_commerce_app/home/components/section_title.dart';
+
 import 'package:e_commerce_app/home/components/special_offers.dart';
-import 'package:e_commerce_app/models/products.dart';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
   @override
